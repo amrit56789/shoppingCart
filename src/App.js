7535b5d7772ffc7a25e9b9d38046e99988d86f6f
amritsingh <amritsingh@innotechteam.in>
2024-02-20 15:09:59 +0530

    shopping-cart

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import ProductListing from "./containers/ProductListing";
+import Header from "./containers/Header";
 import "./App.css";
+import ProductDetails from "./containers/ProductDetails";
 
 function App() {
   return (
     <div className="App">
-     
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<ProductListing />} exact />
+          <Route path="/product/:productId" element={<ProductDetails />} />
+          <Route path="*" element={<div>404 Not Found!</div>} />
+        </Routes>
+      </Router>
     </div>
   );
 }
